cabf04ed5f93efc066f6814dc425b29c6f96f82f
Roberto Castro <roberto.ernesto.castro@gmail.com>
2019-12-09 17:15:12 -0600

    Listos producer de voto Aun falta crear participacion antes de votar  Cambios a ser confirmados: 	modificado:     api/votacion_routes.js

--- api/votacion_routes.js
+++ api/votacion_routes.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const Request = require("request");
+const kafkaProducer = require('../kafka/producer');
 
 
 const url_produccion = 'https://microsvotacion.herokuapp.com/api/votacion/';
@@ -28,7 +29,26 @@ router.get('/afiliacion/', function(req, res){
 });
 
 router.put('/candidato/:id_candidato/:id_mesa/', function(req, res){
-	res.redirect(`${url}candidato/${req.params.id_candidato}/${req.params.id_mesa}`);
+	if (isNaN(parseInt(req.params.id_candidato)) || isNaN(parseInt(req.params.id_mesa))){
+		res.status(400).json({msg: 'Usar parametros numericos'});
+		return;
+	}
+	try{
+		topic = process.env.CLOUDKARAFKA_TOPIC_PREFIX + 'Votar';
+		console.log(topic);
+		let mensaje = {
+			"id_candidato": req.params.id_candidato,
+			"id_mesa": req.params.id_mesa
+		}
+		kafkaProducer.produce(topic, -1, Buffer.from(JSON.stringify(mensaje)), 'votar');
+		res.sendStatus(204);
+	}
+	catch(err){
+		console.log('Error enviando el mensaje', err);
+		res.status(500).json({msg: 'Error enviando el mensaje a Kafka'});
+		return;
+	}
+	//res.redirect(`${url}candidato/${req.params.id_candidato}/${req.params.id_mesa}`);
 });
 
 router.post('/candidato/', function(req,res){
